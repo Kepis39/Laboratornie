61d69c86320b8b359f955fdacceee1db3d523b91
Kepis39 <101018161+Kepis39@users.noreply.github.com>
2026-04-04 13:00:51 +0300

    Add files via upload

---
+++ Коллекции. Динамические структуры данных/Динамические массивы/Program.cs
@@ -0,0 +1,9 @@
+﻿//Вариант 8 средний уровень
+using System.Collections;
+
+ArrayList List = new ArrayList();
+List.AddRange(new double[9]);
+foreach (object o in list) 
+{
+    Console.WriteLine(o);
+}
---
+++ Коллекции. Динамические структуры данных/Коллекции. Динамические структуры данных/Program.cs
@@ -0,0 +1,22 @@
+﻿//вариант 8 средние
+using System.Collections;
+
+Stack stack = new Stack();
+stack.Push("sdf");
+stack.Push("2");
+stack.Push("ssd4");
+stack.Push("hello");
+foreach (string s in stack) Console.WriteLine(s + " ");
+Console.WriteLine();
+stack.Pop();
+stack.Pop();
+foreach (string s in stack) Console.WriteLine(s + " ");
+int min = int.MaxValue;
+foreach (string s in stack)
+{
+   if(int.TryParse(s, out min))
+    {
+        min = s.Length; break;
+    }
+}
+Console.WriteLine($"{min}");
